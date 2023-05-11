26edbb1735363ea1d67661f693926129b0c5be82
Simon C Blyth <simoncblyth@gmail.com>
2023-05-11 15:05:02 +0100

    ScanPE.C ScanPE.sh automate the PE check

--- blyth-88-merge/ScanPE.C
+++ blyth-88-merge/ScanPE.C
@@ -1,6 +1,5 @@
 {
-
-    
-
-
+   TFile* f = new TFile("sample_detsim_user.root") ; 
+   TTree* e = (TTree*)f->Get("evt"); 
+   e->Scan("nPhotons:totalPE:LpmtPE:SpmtPE:NNVTPE:HamaPE:CDPE:WPPE"); 
 }
---
+++ blyth-88-merge/ScanPE.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -l 
+
+cd  /tmp/u4debug/ntds0/
+
+root -l ~/j/blyth-88-merge/ScanPE.C 
+
+
