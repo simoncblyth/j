4faad77e6068fb2366ee081c25bf4a5874aba38d
Simon C Blyth <simoncblyth@gmail.com>
2025-03-25 16:46:08 +0800

    debugging cause of gitlab-ci shell exits from any non-zero rc due to : set -eo pipefail

--- gitlab-ci/mockbuild.sh
+++ gitlab-ci/mockbuild.sh
@@ -9,12 +9,103 @@ mockbuild.sh
     ~/j/gitlab-ci/mockbuild.sh
 
 
-Observations::
+Running mockbuild.sh suggests one cause of 
+gitlab-ci shell exits is line 65 in the below opticks-setup-  
+The [ -n "" ] causes exit when using "set -e" when 
+OPTICKS_SETUP_VERBOSE is not defined::
 
-    [ -n "$OPTICKS_SETUP_VERBOSE" ] && printf 
+     36 opticks-setup- () 
+     37 {
+     38     local mode=${1:-prepend};
+     39     local var=${2:-PATH};
+     40     local dir=${3:-/tmp};
+     41     local st="";
+     42     : dir exists and is not in the path variable already;
+     43     if [ -d "$dir" ]; then
+     44         if [[ ":${!var}:" != *":${dir}:"* ]]; then
+     45             if [ -z "${!var}" ]; then
+     46                 export $var=$dir;
+     47                 st="new";
+     48             else
+     49                 st="add";
+     50                 case $mode in
+     51                     prepend)
+     52                         export $var=$dir:${!var}
+     53                     ;;
+     54                     append)
+     55                         export $var=${!var}:$dir
+     56                     ;;
+     57                 esac;
+     58             fi;
+     59         else
+     60             st="skip";
+     61         fi;
+     62     else
+     63         st="nodir";
+     64     fi;
+     65     [ -n "$OPTICKS_SETUP_VERBOSE" ] && printf "=== %s %10s %10s %20s %s\n" $FUNCNAME $st $mode $var $dir
+     66 }
+
+Googling indicates gitlab-ci uses::
+
+    set -eo pipefail
+
+Investigate with::
+
+    fn0(){ [ -n "$HELLO" ] && printf "hello0\n" ; } 
+    fn1(){ if [ -n "$HELLO" ]; then printf "hello1\n" ; fi ; } 
 
 
-When OPTICKS_SETUP_VERBOSE is not defined the above gives an error with "set -xeo pipefail"
+Default bash::
+
+    epsilon:~ blyth$ fn0(){ [ -n "$HELLO" ] && printf "hello0\n" ; } 
+    epsilon:~ blyth$ fn1(){ if [ -n "$HELLO" ]; then printf "hello1\n" ; fi ; } 
+    epsilon:~ blyth$ fn0
+    epsilon:~ blyth$ fn1
+    epsilon:~ blyth$ HELLO=1
+    epsilon:~ blyth$ fn0
+    hello0
+    epsilon:~ blyth$ fn1
+    hello1
+    epsilon:~ blyth$ HELLO=
+    epsilon:~ blyth$ fn0
+    epsilon:~ blyth$ echo $?
+    1
+    epsilon:~ blyth$ fn1
+    epsilon:~ blyth$ echo $?
+    0
+    epsilon:~ blyth$ HELLO=1
+    epsilon:~ blyth$ fn0
+    hello0
+    epsilon:~ blyth$ echo $?
+    0
+    epsilon:~ blyth$ fn1
+    hello1
+    epsilon:~ blyth$ echo $?
+    0
+    epsilon:~ blyth$ 
+
+
+With "set -e" bash::
+
+    epsilon:~ blyth$ set -e
+    epsilon:~ blyth$ fn0(){ [ -n "$HELLO" ] && printf "hello0\n" ; }
+    epsilon:~ blyth$ fn1(){ if [ -n "$HELLO" ]; then printf "hello1\n" ; fi ; } 
+    epsilon:~ blyth$ HELLO=1
+    epsilon:~ blyth$ fn0
+    hello0
+    epsilon:~ blyth$ echo $?
+    0
+    epsilon:~ blyth$ fn1
+    hello1
+    epsilon:~ blyth$ echo $?
+    0
+    epsilon:~ blyth$ HELLO=
+    epsilon:~ blyth$ fn1
+    epsilon:~ blyth$ echo $?
+    0
+
+    ### fn0 exits the shell when HELLO is empty or unset as the "-n" returns non-zero rc
 
 
 EOU
